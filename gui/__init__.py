c7214ea58a1d1bac01648a6568fa2ac797f4c57a
phsrod <pedrohsr2017@outlook.com>
2025-11-10 09:09:10 -0300

    removi as windows que não estavam sendo utilizadas

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,10 +1,5 @@
 from .login import LoginWindow
 from .home import HomeWindow
-from .clientes import ClientesWindow
-from .servicos import ServicosWindow
-from .funcionarios import FuncionariosWindow
-from .agendamentos import AgendamentosWindow
-from .relatorios import RelatoriosWindow
 from .styles import StyleManager
 
-__all__ = ['LoginWindow', 'HomeWindow', 'ClientesWindow', 'ServicosWindow', 'FuncionariosWindow', 'AgendamentosWindow', 'RelatoriosWindow', 'StyleManager']
+__all__ = ['LoginWindow', 'HomeWindow', 'StyleManager']
